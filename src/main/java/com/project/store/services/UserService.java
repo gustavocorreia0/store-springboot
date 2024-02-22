a266195fd8e1fb7ce5e040e68de6f46b2184e74f
Gustavo Correia <gustavoCorreiaa0@gmail.com>
2024-02-21 21:30:55 -0300

    Exception Handling III

--- src/main/java/com/project/store/services/UserService.java
+++ src/main/java/com/project/store/services/UserService.java
@@ -12,47 +12,53 @@ import com.project.store.repositories.UserRepository;
 import com.project.store.services.exceptions.DatabaseException;
 import com.project.store.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
-	public User registerUser(User newUser){
+
+	public User registerUser(User newUser) {
 		return userRepository.save(newUser);
 	}
-	
+
 	public void deleteUser(Long id) {
-	    try {
-	        if (userRepository.existsById(id)) {
-	            userRepository.deleteById(id);			
-	        } else {				
-	            throw new ResourceNotFoundException(id);			
-	        }		
-	    } catch (DataIntegrityViolationException e) {			
-	        throw new DatabaseException(e.getMessage());		
-	    }	
+		try {
+			if (userRepository.existsById(id)) {
+				userRepository.deleteById(id);
+			} else {
+				throw new ResourceNotFoundException(id);
+			}
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User updateUser(Long id, User userUpdated) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, userUpdated);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, userUpdated);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
-	
+
 	private void updateData(User oldUser, User newUser) {
 		oldUser.setName(newUser.getName());
 		oldUser.setEmail(newUser.getEmail());
 		oldUser.setNumber(newUser.getNumber());
 	}
-	
+
 }
